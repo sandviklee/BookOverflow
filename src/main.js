5842a49cc40c7a4cad580d29800af68fc437c7f9
Simon Sandvik Lee <simonsandviklee@gmail.com>
2023-02-23 02:09:16 +0100

    Implementer prototypen sitt design på nettsiden, og legge til css stylingen: Ref #6

--- src/main.js
+++ src/main.js
@@ -1,26 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import PrimeVue from 'primevue/config';
 
-//Component imports
-import Button from 'primevue/button';
-import Dropdown from 'primevue/dropdown';
-
-//Import themes
-import 'primevue/resources/themes/saga-blue/theme.css';       //theme
-import 'primevue/resources/primevue.min.css';                 //core css
+//Import icons
 import 'primeicons/primeicons.css';                           //icons
 
 
 let app = createApp(App)
 app.use(router)
-app.use(PrimeVue)
 app.mount('#app')
-
-//Add components to app
-app.component('Button', Button);
-app.component('Dropdown', Dropdown);
 
 
 
