ee69327bdea7a958c9f1992d0539f3503510a555
heidihra <heidihra@stud.ntnu.no>
2023-02-23 11:04:07 +0100

    Config firebase; Ref #1

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,6 +1,7 @@
 import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+//import 'firebase/auth';
+//import 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore'
 
 const config = {
     apiKey: "AIzaSyBcPMAVJdmJ3yWADip0ymhN1VatVjUOuZE",
@@ -10,9 +11,8 @@ const config = {
     messagingSenderId: "948092198327",
     appId: "1:948092198327:web:a5cf5eadb4623626671c32",
     measurementId: "G-YW0HE6YYVP"
-};
+}
 
-const firebaseApp = firebase.initializeApp(config);
-
-export const db = firebaseApp.firestore();
-export const auth = firebaseApp.auth();
+const firebaseApp = firebase.initializeApp(config)
+//db kan brukes til å aksessere databasen vår i firestore
+export const db = getFirestore(firebaseApp)
