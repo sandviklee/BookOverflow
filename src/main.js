71858f8a5b49c6c2dc9916fede1fd95000cf783e
Simon Sandvik Lee <simonsandviklee@gmail.com>
2023-02-23 02:09:16 +0100

    Implementer prototypen sitt design på nettsiden, og legge til css stylingen: Ref #6

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import router from './router'
 //Import icons
 import primeicons from 'primeicons/primeicons.css'                           //icons
 
-
 let app = createApp(App)
 app.use(router)
 app.mount('#app')
