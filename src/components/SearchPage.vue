3a58170e71a4234349dfb56d8acc70a9dddea7ba
Simon Sandvik Lee <simonsandviklee@gmail.com>
2023-03-02 13:54:10 +0100

    Fikse filer pga. mistet data: Ref #None

---
+++ src/components/SearchPage.vue
@@ -0,0 +1,53 @@
+<template>
+  <header>
+    <div class="card">
+      <div class="card-content">
+        <div class="content">
+          <h1> 
+            Search page <br>
+            Coming soon! </h1>
+        </div>
+      </div>
+    </div>
+
+  </header>
+</template>
+  
+  
+<script>
+  import { ref, onMounted } from 'vue';
+  //import * as firebase from 'firebase/app';
+  //import 'firebase/database';
+  
+
+  export default {
+    name: 'SearchPage',
+    setup() {
+      const results = ref([]);
+  
+      onMounted(() => {
+        const term = ''; // replace with your search term
+        //const database = firebase.database();
+        //const ref = database.ref('posts');
+
+      });
+  
+      return { results };
+    },
+  };
+</script>
+  
+<style>
+
+@import url('https://fonts.cdnfonts.com/css/lato');
+* {
+  font-family: "lato";
+}
+
+header {
+  padding-top: 5vh;
+  text-align: center;
+  font-weight:700;
+  text-shadow: 1px 0.5px 0px #E98074;
+}
+</style>
---
+++ src/firebase/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBcPMAVJdmJ3yWADip0ymhN1VatVjUOuZE",
+    authDomain: "bookoverflow-561d1.firebaseapp.com",
+    projectId: "bookoverflow-561d1",
+    storageBucket: "bookoverflow-561d1.appspot.com",
+    messagingSenderId: "948092198327",
+    appId: "1:948092198327:web:a5cf5eadb4623626671c32",
+    measurementId: "G-YW0HE6YYVP"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export {
+    db
+}
