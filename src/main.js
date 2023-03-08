77d493b7eff3384052e43d475358ffb018256245
Jonanta <jonanta@gmail.com>
2023-03-08 13:40:21 +0100

    Lagt inn søkeboks i Navbar; Ref #11
    main.js: Importerer InstantSearch, kobler til Vue-app
    App.vue: midlertidig instansearch-kode, fjernes senere
    Navbar.vue: midlertidig instantsearch-kode, oppdateres til å importere
        fra Search.vue
    Search.vue: Lager ny Vue-component for instantsearch, denne er ikke
        koblet til noe enda.
    
    Co-authored-by: Heidi Rasmussen <heidihra@stud.ntnu.no>

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import InstantSearch from 'vue-instantsearch/vue3/es';
 import router from './router'
 
 //Import icons
 import primeicons from 'primeicons/primeicons.css'                           //icons
 
 let app = createApp(App)
+app.use(InstantSearch)
 app.use(router)
 app.mount('#app')
