2bea03dce1e53df00a86fa2c7209a751af561f0c
heidihra <heidihra@stud.ntnu.no>
2023-03-14 14:31:21 +0100

    Skriv kode og testing for legge til bok: Ref#40
    Begynt på kode for å legge til bok og testing av koden.

---
+++ src/javascripts/AddBook.js
@@ -0,0 +1,9 @@
+// import { db } from '../firebase/firebase.js'
+// import { collection, query, where, getDocs } from "firebase/firestore"
+
+function dummyfunction() {
+    return 0;
+}
+
+module.exports.dummyfunction = dummyfunction
+
