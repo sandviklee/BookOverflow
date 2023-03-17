b7d6d2fa98a3eb632d8c61d5904b8930e5c6f510
heidihra <heidihra@stud.ntnu.no>
2023-03-17 13:45:07 +0100

    Kopiere riktig til main: Ref #42

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
+import InstantSearch from 'vue-instantsearch/vue3/es';
 import router from './router'
+import piniaPluginPersistedState from "pinia-plugin-persistedstate"
+
 
 //Import icons
+import primeicons from 'primeicons/primeicons.css'  
 
-
-let app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedState)
+const app = createApp(App)
+app.use(InstantSearch)
 app.use(router)
+app.use(pinia)
 app.mount('#app')
