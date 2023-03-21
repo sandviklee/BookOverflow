b7d4522b45ca2c68015cb7fee3970ad57d66c470
jonanta <jonanta@gmail.com>
2023-03-21 09:43:00 +0100

    Fjernet placeholder SearchPage fra Router: Ref#11

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../components/HomePage.vue'
 import SignupPage from '../components/SignupPage.vue'
 import SignupRegisterPage from '../components/SignupRegisterPage.vue'
-import SearchPage from '../components/SearchPage.vue'
 import ViewAuthor from '../components/ViewAuthorPage.vue'
 import ViewBook from '../components/ViewBookPage.vue'
 import UserList from '../components/UserList.vue'
@@ -13,7 +12,6 @@ import TheLibrary from '../components/TheLibrary.vue'
 
 const routes = [
     { path: '/', component: HomePage },
-    { path: '/search', component: SearchPage },
     { path: '/signup', component: SignupPage, meta: {hideNavbar: true}},
     { path: '/signup/register', component: SignupRegisterPage, meta: {hideNavbar: true}},
     { path: '/author/', component: ViewAuthor },
