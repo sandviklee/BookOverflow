9acacc8b6d2e56e97e1d8a1f31067231cd21b5d0
Simon Sandvik Lee <simonsandviklee@gmail.com>
2023-03-21 22:49:32 +0100

    Finaliser reviews og hvordan reviews ser ut, og lagt til kalkulasjon av gjennomsnitt av vurderinger: Ref #84 & Ref #26

--- src/components/Review.vue
+++ src/components/Review.vue
@@ -1,9 +1,6 @@
 <template>
     <article class="media">
         <figure class="media-left">
-            <p class="image is-64x64">
-            <img src="https://bulma.io/images/placeholders/128x128.png">
-            </p>
         </figure>
         <div class="media-content">
             <div class="content">
@@ -14,19 +11,9 @@
                 <strong>{{ title }}</strong> <small>&ensp;@{{ username }}</small>
                 <br>
                 {{ review }}
-                
             </p>
-
             </div>
-            <nav class="level is-mobile">
-            <div class="level-left">
-                <a class="level-item">
-                <span><i class="pi pi-thumbs-up"></i>&ensp;{{ likeCounter }}</span>
-                </a>
-            </div>
-            </nav>
         </div>
-
     </article>
 </template>
 
@@ -35,7 +22,7 @@ import { ref, onMounted } from 'vue'
 import { doc, getDoc } from "firebase/firestore";
 import { db } from '../firebase/firebase';
 
-//Defines the input for the spesific book, what kind of book should it be?
+//Defines the input for the spesific review
 const props = defineProps({
   reviewInfo: { type: String },
 })
@@ -50,18 +37,15 @@ let title = props.reviewInfo.split(";")[1]
 //Gets the rating from the database.
 let rating = props.reviewInfo.split(";")[2]
 
-//Gets the title from the database.
+//Gets the review from the database.
 let review = props.reviewInfo.split(";")[3]
 
 onMounted(async () => {
     const docRef = doc(db, "users", uid);
     const docSnap = await getDoc(docRef);
-
     let usernameFromDB = docSnap.data().username
     username.value = usernameFromDB;
 });
-
-
 </script>
 
 <style scoped>
--- src/components/ViewBookPage.vue
+++ src/components/ViewBookPage.vue
@@ -77,120 +77,153 @@
               <div v-show="store.uid !== 'no user'" class="if-signed-in">
                 <h1 class="subtitle">CREATE, EDIT OR DELETE YOUR REVIEW:</h1>
 
-                <button
-                type="button"
-                @click="goToReview"
-                :disabled="!createReview"
-                class="button is-link is-medium"><i class="pi pi-plus" style="font-size: 1rem"></i>&ensp;CREATE REVIEW</button>
+                  <button
+                  type="button"
+                  @click="goToReview"
+                  :disabled="!createReview"
+                  class="button is-link is-medium"><i class="pi pi-plus" style="font-size: 1rem"></i>&ensp;CREATE REVIEW</button>
 
-                <button
-                type="button"
-                disabled
-                class="button is-link is-medium"><i class="pi pi-pencil" style="font-size: 1rem"></i>&ensp;EDIT REVIEW</button>
-
-                <button
-                type="button"
-                @click="deleteReview"
-                :disabled="createReview"
-                class="button is-danger is-medium" disabled><i class="pi pi-trash" style="font-size: 1rem"></i>&ensp;DELETE REVIEW</button>
-              </div>
+                  <button
+                  type="button"
+                  @click="deleteReview"
+                  :disabled="createReview"
+                  class="button is-danger is-medium" disabled><i class="pi pi-trash" style="font-size: 1rem"></i>&ensp;DELETE REVIEW</button>
+                </div>
 
 
               <h1 class="subtitle">REVIEWS:</h1>
 
-              <div class="dropdown is-hoverable">
-                <div class="dropdown-trigger">
-                  <button class="button" aria-haspopup="true" aria-controls="dropdown-menu4">
-                    <span>RATING SOURCE</span>
-                    <span class="icon is-small">
-                      <i class="pi pi-chevron-down" aria-hidden="true"></i>
-                    </span>
-                  </button>
-                </div>
-                <div class="dropdown-menu" id="dropdown-menu4" role="menu">
-                  <div class="dropdown-content">
-                    <div class="dropdown-item">
-                      <p>Here you can change the source of ratings.
-                        <hr>
-                        BookOverflow
-                        <hr>
-                        Outsourced
-                      </p>
+                <div class="dropdown is-hoverable">
+                  <div class="dropdown-trigger">
+                    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu4">
+                      <span>RATING SOURCE</span>
+                      <span class="icon is-small">
+                        <i class="pi pi-chevron-down" aria-hidden="true"></i>
+                      </span>
+                    </button>
+                  </div>
+                  <div class="dropdown-menu" id="dropdown-menu4" role="menu">
+                    <div class="dropdown-content">
+                      <div class="dropdown-item">
+                        <p>Here you can change the source of ratings.
+                          <hr>
+                          <button
+                          type="button"
+                          disabled
+                          class="button is-link is-medium">BookOverflow</button>
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
 
-              <div class="dropdown is-hoverable">
-                <div class="dropdown-trigger">
-                  <button class="button" aria-haspopup="true" aria-controls="dropdown-menu4">
-                    <span>Sort by</span>
-                    <span class="icon is-small">
-                      <i class="pi pi-chevron-down" aria-hidden="true"></i>
-                    </span>
-                  </button>
-                </div>
-                <div class="dropdown-menu" id="dropdown-menu4" role="menu">
-                  <div class="dropdown-content">
-                    <div class="dropdown-item">
-                      <p>
-                        Here you can change what you want to sort by:
-                        <hr>
-                        Highest rated
-                        <hr>
-                        Highest likes
-                        <hr>
-                        Lowest rated
-                      </p>
+                <div class="dropdown is-hoverable">
+                  <div class="dropdown-trigger">
+                    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu4">
+                      <span>Sort by</span>
+                      <span class="icon is-small">
+                        <i class="pi pi-chevron-down" aria-hidden="true"></i>
+                      </span>
+                    </button>
+                  </div>
+                  <div class="dropdown-menu" id="dropdown-menu4" role="menu">
+                    <div class="dropdown-content">
+                      <div class="dropdown-item">
+                        <p>
+                          Here you can change what you want to sort by:
+                          <hr>
+                          <p>Reset:</p>
+                          <button
+                          type="button"
+                          @click="changeRatingPage(0)"
+                          class="button is-link is-medium">Reset Rating</button>
+                          <p>Rating:</p>
+                          <ul class="pagination-list">
+                            <li>
+                              <a 
+                              @click="changeRatingPage(1)"
+                              class="pagination-link" id="rating1" aria-label="Page 1" aria-current="page">1</a>
+                            </li>
+                            <li>
+                              <a
+                              @click="changeRatingPage(2)" 
+                              class="pagination-link" id="rating2" aria-label="Goto page 2">2</a>
+                            </li>
+                            <li>
+                              <a
+                              @click="changeRatingPage(3)" 
+                              class="pagination-link" id="rating3" aria-label="Goto page 2">3</a>
+                            </li>
+                            <li>
+                              <a
+                              @click="changeRatingPage(4)" 
+                              class="pagination-link" id="rating4" aria-label="Goto page 2">4</a>
+                            </li>
+                            <li>
+                              <a
+                              @click="changeRatingPage(5)" 
+                              class="pagination-link" id="rating5" aria-label="Goto page 2">5</a>
+                            </li>
+                          </ul>
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
 
-              <div class="paging">
-                <nav class="pagination" role="navigation" aria-label="pagination">
-                  <a 
-                  @click="changeReviewPage(0)"
-                  class="pagination-previous is-disabled" title="This is the first page"
-                   >Previous</a>
-                  <a 
-                  @click="changeReviewPage(1)"
-                  class="pagination-next"
-                  >Next page</a>
-                  <ul class="pagination-list">
-                    <li>
-                      <a 
-                      @click="changeReviewPage(0)"
-                      class="pagination-link is-current" id="page1" aria-label="Page 1" aria-current="page">1</a>
-                    </li>
-                    <li>
-                      <a
-                      @click="changeReviewPage(1)" 
-                      class="pagination-link" id="page2" aria-label="Goto page 2">2</a>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
+                <div class="paging">
+                  <nav class="pagination" role="navigation" aria-label="pagination">
+                    <a 
+                    @click="changeReviewPage(0)"
+                    class="pagination-previous is-disabled" title="This is the first page"
+                    >Previous</a>
+                    <a 
+                    @click="changeReviewPage(1)"
+                    class="pagination-next"
+                    >Next page</a>
+                    <ul class="pagination-list">
+                      <li>
+                        <a 
+                        @click="changeReviewPage(0)"
+                        class="pagination-link is-current" id="page1" aria-label="Page 1" aria-current="page">1</a>
+                      </li>
+                      <li>
+                        <a
+                        @click="changeReviewPage(1)" 
+                        class="pagination-link" id="page2" aria-label="Goto page 2">2</a>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
 
-              <form class="box">
-                <Review v-show="bookPage == 0" class="reviews" v-for="review in reviews[0]" :reviewInfo="review.uid + ';' + review.title + ';' + review.rating + ';' + review.review"/>
-                <Review v-show="bookPage == 1" class="reviews" v-for="review in reviews[1]" :reviewInfo="review.uid + ';' + review.title + ';' + review.rating + ';' + review.review"/>
-              </form>
-              
+                <form class="box">
+                  <Review v-show="reviewPage == 0 && ratingPage == 0" class="reviews" v-for="review in reviewsStandard[0]" :reviewInfo="review.uid + ';' + review.title + ';' + review.rating + ';' + review.review"/>
+                  <Review v-show="reviewPage == 1 && ratingPage == 0" class="reviews" v-for="review in reviewsStandard[1]" :reviewInfo="review.uid + ';' + review.title + ';' + review.rating + ';' + review.review"/>
+                  <Review v-show="reviewPage == 0 && ratingPage == 1" class="reviews" v-for="review in reviewsStar1[0]" :reviewInfo="review.uid + ';' + review.title + ';' + review.rating + ';' + review.review"/>
+                  <Review v-show="reviewPage == 1 && ratingPage == 1" class="reviews" v-for="review in reviewsStar1[1]" :reviewInfo="review.uid + ';' + review.title + ';' + review.rating + ';' + review.review"/>
+                  <Review v-show="reviewPage == 0 && ratingPage == 2" class="reviews" v-for="review in reviewsStar2[0]" :reviewInfo="review.uid + ';' + review.title + ';' + review.rating + ';' + review.review"/>
+                  <Review v-show="reviewPage == 1 && ratingPage == 2" class="reviews" v-for="review in reviewsStar2[1]" :reviewInfo="review.uid + ';' + review.title + ';' + review.rating + ';' + review.review"/>
+                  <Review v-show="reviewPage == 0 && ratingPage == 3" class="reviews" v-for="review in reviewsStar3[0]" :reviewInfo="review.uid + ';' + review.title + ';' + review.rating + ';' + review.review"/>
+                  <Review v-show="reviewPage == 1 && ratingPage == 3" class="reviews" v-for="review in reviewsStar3[1]" :reviewInfo="review.uid + ';' + review.title + ';' + review.rating + ';' + review.review"/>
+                  <Review v-show="reviewPage == 0 && ratingPage == 4" class="reviews" v-for="review in reviewsStar4[0]" :reviewInfo="review.uid + ';' + review.title + ';' + review.rating + ';' + review.review"/>
+                  <Review v-show="reviewPage == 1 && ratingPage == 4" class="reviews" v-for="review in reviewsStar4[1]" :reviewInfo="review.uid + ';' + review.title + ';' + review.rating + ';' + review.review"/>
+                  <Review v-show="reviewPage == 0 && ratingPage == 5" class="reviews" v-for="review in reviewsStar5[0]" :reviewInfo="review.uid + ';' + review.title + ';' + review.rating + ';' + review.review"/>
+                  <Review v-show="reviewPage == 1 && ratingPage == 5" class="reviews" v-for="review in reviewsStar5[1]" :reviewInfo="review.uid + ';' + review.title + ';' + review.rating + ';' + review.review"/>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </header>
-</div>
+    </header>
+  </div>
 </template>
 
 <script setup>
 import { useRoute, useRouter } from 'vue-router';
 import { ref, onMounted } from 'vue'
 import { db } from '../firebase/firebase';
-import { doc, getDoc, getDocs, query, collection, where, addDoc, deleteDoc } from "firebase/firestore"; 
+import { doc, getDoc, getDocs, query, collection, where, deleteDoc, updateDoc } from "firebase/firestore"; 
 import { userStore } from '../stores/UsersStore';
 import Review from './Review.vue';
 
@@ -204,14 +237,27 @@ const imgUrl = ref()
 const publishedText = ref()
 const bookGenres = ref([])
 const bookAwards = ref([])
-const reviews = ref([])
+const reviewsStandard = ref([])
+const reviewsStar1 = ref([])
+const reviewsStar2 = ref([])
+const reviewsStar3 = ref([])
+const reviewsStar4 = ref([])
+const reviewsStar5 = ref([])
 const bookRating = ref()
-const bookPage = ref(0)
+const reviewPage = ref(0)
+const ratingPage = ref(0)
 
 let createReview = true;
 let book = route.params.id;
 let reviewId = ''
+let authorId = ''
+let reviews;
 
+/**
+ * Slices an array into a 2D array with max sized chunks.
+ * @param {*} arr       is the array.
+ * @param {*} chunkSize is the max size of the inner array.
+ */
 function sliceIntoChunks(arr, chunkSize) {
     const res = [];
     for (let i = 0; i < arr.length; i += chunkSize) {
@@ -221,6 +267,72 @@ function sliceIntoChunks(arr, chunkSize) {
     return res;
 }
 
+/**
+ * Adds the specific rating to a ref.
+ * @param {*} array  is the array you want to get ratings from.
+ * @param {*} rating is the rating you want to check for.
+ * @param {*} ref    is the ref you want to add the ratings to.
+ */
+function addRatingToRef(array, rating, ref) {
+  let finishedArray = []
+  array.forEach(element => {
+    if (element.rating == rating) {
+      finishedArray.push(element);
+    };
+  });
+  ref.value = sliceIntoChunks(finishedArray, 5);
+}
+
+/**
+ * Calculates the average rating of the book from reviews.
+ */
+async function calculateAvgRating() {
+  let avgRating = 0;
+  reviews.forEach(review => {
+    avgRating = avgRating + review.rating;
+  });
+  avgRating = avgRating/reviews.length;
+
+  if (avgRating == bookRating.value) {
+    return
+  }
+  const bookRef = doc(db, "books", book);
+  
+  await updateDoc(bookRef, {
+    avgRating: avgRating
+  });
+}
+
+/**
+ * Changes the rating page.
+ * @param {*} pageNumber is the number of the page.
+ */
+function changeRatingPage(pageNumber) {
+  ratingPage.value = pageNumber;
+}
+
+/**
+ * deletes the review.
+ */
+async function deleteReview() {
+  await deleteDoc(doc(db, "reviews", reviewId));
+  window.location.reload()
+}
+
+/**
+ * Change page to the write review page.
+ * Checks if the user is logged in.
+ */
+function goToReview() {
+  if (store.uid !== 'no user') {
+    router.push('/review/' + book)
+  }
+}
+
+/**
+ * Changes the review page.
+ * @param {*} pageNumber is the number of the page.
+ */
 function changeReviewPage(pageNumber) {
   if (pageNumber == 1) {
     document.getElementById("page2").className = 'pagination-link is-current';
@@ -229,13 +341,14 @@ function changeReviewPage(pageNumber) {
     document.getElementById("page1").className = 'pagination-link is-current';
     document.getElementById("page2").className = 'pagination-link';
   }
-  bookPage.value = pageNumber;
+  reviewPage.value = pageNumber;
 }
 
-//Retrives book information from the ID when you click on a book.
+/**
+ * On mounted function to retrieve everything we need on start.
+ */
 onMounted(async () => { 
   const q = query(collection(db, "reviews"), where("book.id", "==", book));
-
   const querySnapshot = await getDocs(q);
   const reviewArray = [];
   querySnapshot.forEach((doc) => {
@@ -251,9 +364,14 @@ onMounted(async () => {
       }
       reviewArray.push(review)
   });
-  
-  reviews.value = sliceIntoChunks(reviewArray, 5);
-  
+  reviews = reviewArray;
+  reviewsStandard.value = sliceIntoChunks(reviewArray, 5);
+  addRatingToRef(reviewArray, 1, reviewsStar1);
+  addRatingToRef(reviewArray, 2, reviewsStar2);
+  addRatingToRef(reviewArray, 3, reviewsStar3);
+  addRatingToRef(reviewArray, 4, reviewsStar4);
+  addRatingToRef(reviewArray, 5, reviewsStar5);
+
   //Get book information
   const docBook = await doc(db, 'books', book);
   const docSnapBook = await getDoc(docBook);
@@ -276,30 +394,21 @@ onMounted(async () => {
     publishedText.value = published;
     bookRating.value = rating;
     authorId = aid;
-    awards.forEach(award => {
+    if (awards) {
+      awards.forEach(award => {
       bookAwards.value.push(award)
     });
-    genres.forEach(genre => {
+    }
+    if (genres) {
+      genres.forEach(genre => {
       bookGenres.value.push(genre)
     });
-
+    }
   } else {
     console.log("No such document!");
   }
-
-  console.log(reviews.value[1]);
+  calculateAvgRating();
 })
-
-async function deleteReview() {
-  await deleteDoc(doc(db, "reviews", reviewId));
-  window.location.reload()
-}
-
-function goToReview() {
-  if (store.uid !== 'no user') {
-    router.push('/review/' + book)
-  }
-}
 
 </script>
 
@@ -446,8 +555,9 @@ header {
   height: 0.5vh;
   width: 95%;
   left: 3.5vh;
-  
 }
 
-
+.dropdown-content {
+  width: 28vh;
+}
 </style>
